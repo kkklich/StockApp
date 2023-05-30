e57ca53084c49b8109a04037fd0fa7401747e734
krzysztof.klich@idealogic.pl <krzysztof.klich@idealogic.pl>
2023-05-30 23:10:31 +0200

    try to add backend sever

---
+++ StockAppBackaned/StockAppBackaned/Controllers/CompanyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockAppBackaned.Controllers
+{
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CompanyController : ControllerBase
+	{
+		public CompanyController() { }
+
+
+		[AllowAnonymous]
+		[HttpGet("GetNumber")]
+		public double GetNumber()
+		{
+			return 25;
+		}
+	}
+}
