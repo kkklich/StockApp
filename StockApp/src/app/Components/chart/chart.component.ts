609a3720a72530b14abfef1a19132cdcbe96c5ee
krzysztof.klich@idealogic.pl <krzysztof.klich@idealogic.pl>
2023-08-11 00:05:07 +0200

    add selctor for SMA line

--- StockApp/src/app/Components/chart/chart.component.ts
+++ StockApp/src/app/Components/chart/chart.component.ts
@@ -11,13 +11,17 @@ import { PatternService } from 'src/app/Services/pattern.service';
 })
 export class ChartComponent implements OnInit {
 
-  private quantityOfStockValue = 100;
+  public timeStock: { time: string, numberOfDays: number }[] = [{ time: '1M', numberOfDays: 20 }, { time: '2M', numberOfDays: 40 }, { time: '3M', numberOfDays: 60 }, { time: '6M', numberOfDays: 120 }, { time: '1R', numberOfDays: 240 }, { time: '3R', numberOfDays: 720 }, { time: 'MAX', numberOfDays: 99999 }];
+  public quantityOfStockValue = this.timeStock[1].numberOfDays;
   public chart: any;
   public recordsStocks: stockData[] = [];
 
   private dateTimeArray: string[] = [];
   private stockValueArray: number[] = [];
-  private averageRolling: number[] = [];
+  private averageRolling: any[] = [];
+  public lengthArray: number[] = [10, 20, 30, 50];
+  public lengthChartSMA: number = 10;
+  private loadedStockData: stockData[] = [];
 
   constructor(private loadCSVService: LoadCSVService,
     private patternService: PatternService) { }
@@ -28,25 +32,31 @@ export class ChartComponent implements OnInit {
 
   private loadData() {
     this.loadCSVService.getRecordsSubscribe().subscribe(res => {
-      const dataStock = res.slice(-1 * this.quantityOfStockValue);
-      this.dateTimeArray = dataStock.map(x => x.date.toLocaleDateString('pl-PL'));
-
-      this.stockValueArray = dataStock.map(x => x.close);
-      this.calculatePattern();
-      this.createLineChart();
+      this.loadedStockData = res;
+      this.applyChart();
     })
-
   }
 
-  calculatePattern() {
-    this.averageRolling = this.patternService.calculateAverageRolling(this.stockValueArray, 10);
-    console.log(this.averageRolling);
+  public applyChart() {
+    const dataStock = this.loadedStockData.slice(-1 * this.quantityOfStockValue);
+    this.dateTimeArray = dataStock.map(y => y.date.toLocaleDateString('pl-PL'));
+    this.stockValueArray = dataStock.map(x => x.close);
+
+    this.calculatePattern();
+    this.createLineChart();
+  }
+
+  private calculatePattern() {
+    this.averageRolling = this.patternService.calculateAverageRolling(this.stockValueArray, this.lengthChartSMA);
+    this.averageRolling.unshift(...new Array(this.lengthChartSMA).fill(null));
   }
 
 
   private createLineChart(): void {
     if (this.chart != undefined)
       this.chart.destroy();
+
+    const nameLine = "SMA " + this.lengthChartSMA;
 
     const fileName = this.loadCSVService.fileTitle;
     this.chart = new Chart("chart", {
@@ -58,12 +68,17 @@ export class ChartComponent implements OnInit {
           data: this.stockValueArray,
         },
         {
-          label: "xd",
+          label: nameLine,
           data: this.averageRolling,
         }],
       },
       options: {
-        responsive: true
+        responsive: true,
+        elements: {
+          point: {
+            radius: 0
+          }
+        }
       }
     });
   }
